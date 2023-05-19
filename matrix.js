8cb2ac7305043b78f06ecc0037c4caa2b823371d
MasterSun8 <kacpi.doro@gmail.com>
2023-05-19 12:53:52 +0200

    lesgo

--- matrix.js
+++ matrix.js
@@ -1,7 +1,7 @@
 const max = 100
 const min = 1
 
-function matrix(row, col=null){
+function matrix(row, col=null, a=198, b=99){
     col = col == null   ? row : col
     row = row >= min    ? row : null
     col = col >= min    ? col : null
@@ -12,78 +12,104 @@ function matrix(row, col=null){
         console.log("Incorrect input")
         return
     }
-    let matrix = Array.from({length: row}, () => Array.from({length: col}, () => Math.floor(Math.random() * 198 - 99)))
+    let matrix = Array.from({length: row}, () => Array.from({length: col}, () => Math.floor(Math.random() * a - b)))
     return matrix
 }
 
-function determinant(matrix, sq=false){
+function determinant(matrix, sq=false, prec=false){
     console.log(matrix)
-    if(!Array.isArray(matrix) || !Array.isArray(matrix[0]) || Array.isArray(matrix[0][0])){
-        console.log("Incorrect input")
-        throw new Error("The passed data isn't a matrix")
-    }
+    isMatrix(matrix)
     let row = matrix.length
     let col = matrix[0].length
     sq = sq ? sq : row==col
     if(sq){
-        let det = 1
-        for(let i = 0; i < row; i++){
-            let pivot = matrix[i][i]
-            if(pivot == 0){
-                let found = false
-                for(let j = i+1; j < row; j++){
-                    if(matrix[j][i] != 0){
-                        found = true
-                        let temp = matrix[i]
-                        matrix[i] = matrix[j]
-                        matrix[j] = temp
-                        pivot = matrix[i][i]
-                        det *= -1
-                        break
+        if(!prec){
+            let det = 1
+            for(let i = 0; i < row; i++){
+                let pivot = matrix[i][i]
+                if(pivot == 0){
+                    let found = false
+                    for(let j = i+1; j < row; j++){
+                        if(matrix[j][i] != 0){
+                            found = true
+                            let temp = matrix[i]
+                            matrix[i] = matrix[j]
+                            matrix[j] = temp
+                            pivot = matrix[i][i]
+                            det *= -1
+                            break
+                        }
+                    }
+                    if(!found){
+                        return 0
                     }
                 }
-                if(!found){
-                    return 0
+                for(let j = i+1; j < row; j++){
+                    let factor = matrix[j][i] / pivot
+                    for(let k = i+1; k < col; k++){
+                        matrix[j][k] -= factor * matrix[i][k]
+                    }
                 }
+                det *= pivot
             }
-            for(let j = i+1; j < row; j++){
-                let factor = matrix[j][i] / pivot
-                for(let k = i+1; k < col; k++){
-                    matrix[j][k] -= factor * matrix[i][k]
-                }
-            }
-            det *= pivot
-        }
-        console.log(matrix)
+            console.log(matrix)
+    
+            return det
+        }else{
+            let result = 0
 
-        return det
-        /*
-        let result = 0
-
-        for(let i = 0; i<col; i++){
-            let sign = (i % 2 == 0) ? 1 : -1
-            let temp = new Array
-            for(let j = 1; j < row; j++){
-                let t = new Array
-                    for(let k = 0; k < col; k++){
-                        if(k == i){
-                            continue
-                        }
-                        t.push(matrix[j][k])
+            for(let i = 0; i<col; i++){
+                let sign = (i % 2 == 0) ? 1 : -1
+                let temp = new Array
+                for(let j = 1; j < row; j++){
+                    let t = new Array
+                        for(let k = 0; k < col; k++){
+                            if(k == i){
+                                continue
+                            }
+                            t.push(matrix[j][k])
+                    }
+                    temp.push(t)
                 }
-                temp.push(t)
+                result += sign * matrix[0][i] * determinant(temp, true, truez)
             }
-            result += sign * matrix[0][i] * determinant(temp)
+            return result    
         }
-        return result*/
     }else{
         console.log("The matrix isnt square")
         return false
     }
 }
 
+function multiplication(matrixA, matrixB){
+    let mA
+    let mB
+    if(!Number.isInteger(matrixA)){
+        mA = isMatrix(matrixA)
+    }
+    if(!Number.isInteger(matrixB)){
+        mB = isMatrix(matrixB)
+    }
+    let rowA = matrixA.length()
+    let colA = matrixA[0].length()
+    let rowB = matrixB.length()
+    let colB = matrixB[0].length()
+    
+    let matrix = matrix(colA)
+}
+
+
+
+
+
+
+
+
+
+
+
 function drawMatrix(matrix){
-    if(!Array.isArray(matrix) || !Array.isArray(matrix[0]) || Array.isArray(matrix[0][0])){
+    if(isMatrix(matrix)){
         console.log("Incorrect input")
         throw new Error("The passed data isn't a matrix")
     }
@@ -102,4 +128,11 @@ function drawMatrix(matrix){
     })
     console.log(string)
 
+}
+
+function isMatrix(matrix){
+    if(!Array.isArray(matrix) || !Array.isArray(matrix[0]) || Array.isArray(matrix[0][0])){
+        throw new Error("The passed data isn't a matrix")
+    }
+    return true
 }
